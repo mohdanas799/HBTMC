33c316916ac5d00fe540d82316180436079d4238
Mohd Anas <anasmohd4697@gmail.com>
2024-11-17 00:46:55 +0530

    new commit

--- src/pages/Anatomy.jsx
+++ src/pages/Anatomy.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import { FaPhoneAlt, FaEnvelope, FaSearch } from "react-icons/fa";
+import Navbar2 from "../components/Navbar2";
+
+import Gallery2 from "../components/Gallery2";
+import InfoSections from "../components/Section1";
+import TimeTable from "../components/TimeTable";
+import Activities from "../components/Activities";
+import Teaching from "../components/Teaching";
 
 const Anatomy = () => {
   return (
-    <>
-    
-    </>
-  )
-}
+    <div>
+      <Navbar2 />
+      <Teaching />
+      <Activities />
+      <TimeTable />
+      <TimeTable />
+      <TimeTable />
+      <Activities />
+      <Gallery2 />
+      <InfoSections />
+    </div>
+  );
+};
 
-export default Anatomy
+export default Anatomy;
