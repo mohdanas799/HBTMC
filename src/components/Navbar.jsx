e9d5e54294ccc53c80557cbdbdd66f57b95a7815
Mohd Anas <anasmohd4697@gmail.com>
2024-11-24 01:06:31 +0530

    newcioe

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
           <img
             src="https://i.ibb.co/z611bTt/Untitled-1-1.png"
             alt="Left Logo"
-            className="w-16 h-16 xl:hidden"
+            className="w-16 h-16 xxl:hidden "
           />
             <h1 className="text-2xl dm:text-lg font-sans font-bold">
               Hinduhridaysamrat Balasaheb Thackeray Medical College <br />
